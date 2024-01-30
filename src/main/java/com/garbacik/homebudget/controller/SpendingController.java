e76be9bf9614230b62decf65befc09ef27c46b11
Paweł Jończyk <pawel.jonczyk@asseco.pl>
2024-01-31 00:55:16 +0100

    added swagger

--- src/main/java/com/garbacik/homebudget/controller/SpendingController.java
+++ src/main/java/com/garbacik/homebudget/controller/SpendingController.java
@@ -2,6 +2,9 @@ package com.garbacik.homebudget.controller;
 
 import com.garbacik.homebudget.model.Spending;
 import com.garbacik.homebudget.service.SpendingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,32 +13,40 @@ import java.util.List;
 @RestController
 @RequestMapping("/spending")
 @CrossOrigin
+@Api(tags = "Home Budget API", description = "Essential controller for CRUD operations")
 public class SpendingController {
     @Autowired
     private SpendingService spendingService;
 
+    @ApiOperation(value = "Add spending")
     @PostMapping("/add")
     public Spending add(@RequestBody Spending spending){
         return spendingService.saveSpending(spending);
     }
 
+    @ApiOperation(value = "Get list of all spendings")
     @GetMapping("/getAll")
     public List<Spending> list(){
         return spendingService.getAllSpendings();
     }
 
+    @ApiOperation(value = "Get spending by Id")
     @GetMapping("/get/{id}")
-    public Spending getSpendingById(@PathVariable(value = "id") Integer id){
+    public Spending getSpendingById(@PathVariable(value = "id")
+                                        @ApiParam(value = "ID of spending", required = true) Integer id){
         return spendingService.getSpendingById(id);
     }
 
+    @ApiOperation(value = "Update spending existing in database")
     @PutMapping("/update")
     public Spending updateSpending(@RequestBody Spending spending){
         return spendingService.updateSpending(spending);
     }
 
+    @ApiOperation(value = "Delete spending by Id")
     @DeleteMapping("/delete/{id}")
-    public void deleteSpendingById(@PathVariable(value = "id") Integer id){
+    public void deleteSpendingById(@ApiParam(value = "ID of spending", required = true)
+                                       @PathVariable(value = "id") Integer id){
         spendingService.deleteSpendingById(id);
     }
 }
--- src/main/java/com/garbacik/homebudget/model/Spending.java
+++ src/main/java/com/garbacik/homebudget/model/Spending.java
@@ -1,17 +1,25 @@
 package com.garbacik.homebudget.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@ApiModel(description = "Details about the spending")
 public class Spending {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "The unique ID of the spending")
     private int id;
+    @ApiModelProperty(notes = "The name of the spending")
     private String name;
+    @ApiModelProperty(notes = "The cash value of the spending")
     private String value;
+    @ApiModelProperty(notes = "The category of the spending")
     private String category;
 
 
